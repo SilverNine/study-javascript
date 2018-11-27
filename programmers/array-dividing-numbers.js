647cc469f00e8c35f9b274d4d19907409112473a
SilverNine <>
2018-11-27 13:41:55 +0900

    arrays stream

---
+++ programmers/array-dividing-numbers.js
@@ -0,0 +1,17 @@
+function solution(arr, divisor) {
+  var answer = []
+
+  arr.forEach(element => {
+    if (element % divisor == 0) {
+      answer.push(element)
+    }
+  })
+
+  if (answer.length == 0) {
+    answer.push(-1)
+  }
+
+  return answer.sort((a, b) => a - b)
+}
+
+console.log(solution([5, 9, 7, 10], 5))
